424f9b9d6f2ddd515de1e7b3d0b0f2d409449a16
Abdulrahman <a.atiyah.sa@gmail.com>
2021-10-09 22:20:18 +0100

    Create new Model (Teacher)

---
+++ RainbowSchool/Model/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RainbowSchool.Model
+{
+    class Teacher
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int ClassNo { get; set; }
+        public string Section{ get; set; }
+    }
+}
